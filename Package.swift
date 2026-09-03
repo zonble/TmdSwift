ca80d61b51c85a007a43f8d0b5ae21dbe0ca993c
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-09-03 16:19:26 +0800

    Downgrade swift-tools-version to 5.10 for CI and Mint compatibility

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -73,5 +73,5 @@ let package = Package(
             dependencies: ["TmdSwift", "TmdMIDI", "TmdMusicXML", "TmdLilyPond", "TmdUtils"]
         ),
     ],
-    swiftLanguageModes: [.v6]
+    swiftLanguageVersions: [.v5]
 )
