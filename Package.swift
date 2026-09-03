c3b94ccf9aadad584f06f71abd5917af3cba52d8
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-09-03 16:22:33 +0800

    Upgrade to swift-tools-version 6.0 and adopt macos-latest/ubuntu-latest CI runners

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -73,5 +73,5 @@ let package = Package(
             dependencies: ["TmdSwift", "TmdMIDI", "TmdMusicXML", "TmdLilyPond", "TmdUtils"]
         ),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v6]
 )
